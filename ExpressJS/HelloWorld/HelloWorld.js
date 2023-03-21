4e6bfa75fa5a70f9c8a3e2b30ee005138383730b
XII <ur12xii@gmail.com>
2023-03-22 06:44:38 +0900

    [studyExpress][Update] HelloWorld Seerver Script & description

--- ExpressJS/HelloWorld/HelloWorld.js
+++ ExpressJS/HelloWorld/HelloWorld.js
@@ -8,5 +8,7 @@ express.get('/', // '/' URL로 get 메소드 요청을 보낼 경우
 })
 
 express.listen(port, () => { // 포트 연결
+  // 성공 시 실행하는 구문
   console.log(`Example app listening on port ${port}`) // 터미널에 Example app listening on port ${port} 메시지를 띄움
+  // chrome에 접속해서 URL 창에 localhost:3000 을 입력해보세요! Hello 
 })
